48ce8e7b954f6795a6acfd31b5d9de4de0cb7df5
Rondi650 <rondinellenunes123@gmail.com>
2025-10-05 14:05:29 -0300

    exercicios pedra papel e  tesoura

--- PROJETOS/exercicios/Pedra, papel e tesoura.py
+++ PROJETOS/exercicios/Pedra, papel e tesoura.py
@@ -38,26 +38,26 @@ else:
     
 '''SOLUCAO OFICIAL ALURA'''
 import random 
-def pedra_papel_tesoura():
-opcoes = ["pedra", "papel", "tesoura"] 
-escolha_computador = random.choice(opcoes) 
-escolha_usuario = input("Escolha: pedra, papel ou tesoura? ").lower() 
+def pedra_papel_tesoura2():
+    opcoes = ["pedra", "papel", "tesoura"] 
+    escolha_computador = random.choice(opcoes) 
+    escolha_usuario = input("Escolha: pedra, papel ou tesoura? ").lower() 
 
-if escolha_usuario not in opcoes: 
-    print("Escolha inválida!") 
-    return 
+    if escolha_usuario not in opcoes: 
+        print("Escolha inválida!") 
+        return 
 
-print(f"Computador escolheu: {escolha_computador}") 
+    print(f"Computador escolheu: {escolha_computador}") 
 
-if escolha_usuario == escolha_computador: 
-    print("Empate!") 
-elif ( 
-    (escolha_usuario == "pedra" and escolha_computador == "tesoura") or 
-    (escolha_usuario == "papel" and escolha_computador == "pedra") or 
-    (escolha_usuario == "tesoura" and escolha_computador == "papel") 
-): 
-    print("Você venceu!") 
-else: 
-    print("Você perdeu!") 
+    if escolha_usuario == escolha_computador: 
+        print("Empate!") 
+    elif ( 
+        (escolha_usuario == "pedra" and escolha_computador == "tesoura") or 
+        (escolha_usuario == "papel" and escolha_computador == "pedra") or 
+        (escolha_usuario == "tesoura" and escolha_computador == "papel") 
+    ): 
+        print("Você venceu!") 
+    else: 
+        print("Você perdeu!") 
  
-pedra_papel_tesoura()
+pedra_papel_tesoura2()
